c47cbebca8d5037d7b5b79c51bb768f7b3df88e0
Gustavo Feriani <guuh.feriani@gmail.com>
2023-10-23 15:44:41 -0300

    update JavaScript
    att do projeto senai Midi

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// alert('Ola mundo'); teste de script
+
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+// clap
+function tocaSomClap () {
+    document.querySelector('#som_tecla_clap').play();
+}
+document.querySelector('.tecla_clap').onclick = tocaSomClap;
+
+
+
+
+
+
+
+
+
+
+
+
